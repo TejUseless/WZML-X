f83abf58ca1d29b3a5768f07ff63c03edc4005e9
MUNTHAMALA TEJA <94234215+TejUseless@users.noreply.github.com>
2024-11-30 12:29:57 +0530

    Update tasks_listener.py
    Signed-off-by: MUNTHAMALA TEJA <94234215+TejUseless@users.noreply.github.com>

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -42,7 +42,7 @@ from bot.helper.themes import BotTheme
 
 
 class MirrorLeechListener:
-    def __init__(self, message, compress=False, extract=False, isQbit=False, isLeech=False, tag=None, select=False, seed=False, sameDir=None, rcFlags=None, upPath=None, isClone=False, 
+    def __init__(self, message, compress=False, extract=False,retag=False isQbit=False, isLeech=False, tag=None, select=False, seed=False, sameDir=None, rcFlags=None, upPath=None, isClone=False, 
                 join=False, drive_id=None, index_link=None, isYtdlp=False, source_url=None, logMessage=None, leech_utils={}):
         if sameDir is None:
             sameDir = {}
@@ -51,6 +51,7 @@ class MirrorLeechListener:
         self.excep_chat = bool(str(message.chat.id) in config_dict['EXCEP_CHATS'].split())
         self.extract = extract
         self.compress = compress
+        self.retag = retag
         self.isQbit = isQbit
         self.isLeech = isLeech
         self.isClone = isClone
